ddd2d7d03148aa201136468a09723800fb588d79
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-04-28 16:35:16 -0400

    added NavBtnLink to NavbarElements and added styles

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -94,3 +94,17 @@ align-items: center;
     display: none;
 }
 `
+
+export const NavBtnLink = styled(LinkRouter)`
+border-radius: 50px;
+background: #01bf71;
+white-space: nowrap;
+padding: 10px 22px;
+color: #010606;
+font-size: 16px;
+outline: none;
+border: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+`
