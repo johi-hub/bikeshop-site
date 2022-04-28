df2ea95bf84d31d2a8156844abf2b011a05225e7
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-04-28 16:29:52 -0400

    modified NavLogo color from red to white

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -30,7 +30,7 @@ export const NavbarContainer = styled.div`
 `
 
 export const NavLogo = styled(LinkRouter)`
-color: red;
+color: #fff;
 justify-self: flex-start;
 cursor: pointer;
 font-size: 1.5rem;
