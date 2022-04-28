90cd2258380af2cc1ddf5927d2bf2ab1cabf88f5
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-04-28 17:17:40 -0400

    added sidebar links and button to index.js and commented out the opacity and top from SideBarElement

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,7 +6,26 @@ const Sidebar = () => {
     <SidebarContainer>
         <Icon>
             <CloseIcon/>
-        </Icon>
+          </Icon>
+          <SidebarWrapper>
+              <SidebarMenu>
+                  <SidebarLink to="about">
+                      About
+                  </SidebarLink>
+                  <SidebarLink to="discover">
+                      Discover
+                  </SidebarLink>
+                  <SidebarLink to="services">
+                      Services
+                  </SidebarLink>
+                  <SidebarLink to="signup">
+                      Sign Up
+                  </SidebarLink>
+              </SidebarMenu>
+              <SideBtnWrap>
+                  <SidebarRoute to="/signin">Sign In</SidebarRoute>
+              </SideBtnWrap>
+          </SidebarWrapper>
     </SidebarContainer>
   )
 }
