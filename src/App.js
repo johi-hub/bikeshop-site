95b1b009660898a9746e4014e5fc12fc13bc85a3
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-04-28 18:02:20 -0400

    created an index.js in pages and moved sidebar and navbar routes in app.js to index.js of pages and named it home. Routed home in the app.js instead.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-      <Sidebar/>
-      <Navbar/>
+      <Home/>
     </Router>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+      <>
+          <Sidebar />
+          <Navbar/>
+      </>
+  )
+}
+
+export default Home
