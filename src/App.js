3e54df1111f4c3e5f892607b6c4c827893d962c4
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-04-28 20:13:45 -0400

    fixed the imports for the Sidebar and Navbar in the index.js pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './pages/index';
 
 function App() {
   return (
