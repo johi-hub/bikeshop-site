9cd91c6c07a355987e7ae6635c797db027ac393b
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-05-09 19:09:51 -0400

    added data to info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Gizmos and things',
+    headline: 'Thingamajigs with wheels',
+    description: 'Start using some aboslute units with wheels that make you seem less square more well-rounded',
+    buttonLabel: 'Check It Out',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Longboard',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '../ButtonElement';
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElement';
 
-const InfoSection = () => {
+const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headline, darkText, description, buttonLabel, img, alt}) => {
   return (
     <>
-          <InfoContainer id={id}>
+          <InfoContainer lightBg={lightBg} id={id}>
               <InfoWrapper>
-                  <InfoRow>
+                  <InfoRow imgStart={imgStart}>
                       <Column1>
                           <TextWrapper>
-                              <TopLine>TopLine</TopLine>
-                              <Heading>Heading</Heading>
-                              <Subtitle>Subtitle</Subtitle>
+                              <TopLine>{topLine}</TopLine>
+                              <Heading lightText={lightText}>{headline}</Heading>
+                              <Subtitle darkText={darkText}>{description}</Subtitle>
                               <BtnWrap>
-                                  <Button to='home'> Button </Button>
+                                  <Button to='home'
+                                      smooth={true}
+                                      duration={500}
+                                      spy={true}
+                                      exact="true"
+                                      offset={-80}
+                                  >{buttonLabel}</Button>
                               </BtnWrap>
                             </TextWrapper>
                       </Column1>
+                      <Column2>
+                          <ImgWrap>
+                              <Img src={img} alt={alt} />
+                          </ImgWrap>
+                      </Column2>
                   </InfoRow>
               </InfoWrapper>
         </InfoContainer>
