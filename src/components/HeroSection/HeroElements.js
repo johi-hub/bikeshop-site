9a41a223b45eb481e01835abbb94c27d2cbb63bb
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-05-09 12:15:21 -0400

    stylized HeroH1

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -30,3 +30,26 @@ height: 100%;
 object-fit: cover;
 background: #232a34;
 `
+
+export const HeroContent = styled.div`
+z-index: 3;
+max-width: 1200px;
+position: absolute;
+padding: 8px 24px;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const HeroH1 = styled.h1`
+color: #fff;
+font-size: 48px;
+text-align: center;
+
+@media screen and (max-width: 768px) {
+    font-size: 40px;
+}
+
+@media screen and (max-width: 480px) {
+    font-size: 32px;
+}
+`
