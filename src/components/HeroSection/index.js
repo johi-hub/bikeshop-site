b398d4d2119885155236f1e55c248560b24ef245
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2022-05-09 11:57:48 -0400

    added H1, paragraph, and button to Hero Section. Stylized the button to change when hoveredover

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -7,7 +7,16 @@ const HeroSection = () => {
       <HeroContainer>
           <HeroBg>
               <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-          </HeroBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Your Expected Site When Expecting</HeroH1>
+        <HeroP>Sign up for a new account today and recieve information and support from those like you!</HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup">
+            Get started {hover ? <ArrowForward/> : <ArrowRight/>}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
